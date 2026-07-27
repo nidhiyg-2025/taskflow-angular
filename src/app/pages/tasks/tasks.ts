cbba591b5c4a0744df978f25322ca274ddd4245b
nidhiyg-2025 <nidhiyenagandula@gmail.com>
2026-07-27 13:28:10 +0530

    create tasks page

--- src/app/pages/tasks/tasks.ts
+++ src/app/pages/tasks/tasks.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Task {
+  id: number;
+  title: string;
+  priority: 'High' | 'Medium' | 'Low';
+  status: 'Pending' | 'In Progress' | 'Completed';
+  dueDate: string;
+}
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tasks.html',
-  styleUrl: './tasks.css',
+  styleUrl: './tasks.css'
 })
 export class Tasks {
+
+  tasks: Task[] = [
+    {
+      id: 1,
+      title: 'Design Authentication Screen',
+      priority: 'High',
+      status: 'Completed',
+      dueDate: '28 Jul 2026'
+    },
+    {
+      id: 2,
+      title: 'Build Dashboard Layout',
+      priority: 'Medium',
+      status: 'In Progress',
+      dueDate: '30 Jul 2026'
+    },
+    {
+      id: 3,
+      title: 'Develop Analytics Module',
+      priority: 'Low',
+      status: 'Pending',
+      dueDate: '02 Aug 2026'
+    }
+  ];
 
 }
